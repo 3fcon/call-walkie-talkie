c0a228ef5df534c6fb2e655f2e7d13ffa9ee17f6
3fcon <l0219l0219l@gmail.com>
2026-09-14 01:42:10 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,45 +1,43 @@
 plugins {
-    id("com.android.application") version "8.5.2"
-    id("org.jetbrains.kotlin.android") version "1.9.24"
+    id 'com.android.application'
+    id 'org.jetbrains.kotlin.android'
+    // Optional: Add Google services plugin if google-services.json is present in app/
+    // id 'com.google.gms.google-services'
 }
 
 android {
-    namespace = "com.bulletproof.call"
-    compileSdk = 34
+    namespace 'com.bulletproof.call'
+    compileSdk 34
 
     defaultConfig {
-        applicationId = "com.bulletproof.call"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 2
-        versionName = "2.0-WS"
+        applicationId 'com.bulletproof.call'
+        minSdk 24
+        targetSdk 34
+        versionCode 1
+        versionName '1.0'
     }
 
-    buildFeatures {
-        compose = true
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
     }
-    
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
-    
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
     }
-    
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = '1.8'
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.ui:ui:1.6.2")
-    implementation("androidx.compose.foundation:foundation:1.6.2")
-    implementation("androidx.compose.material3:material3:1.2.0")
-    
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation 'androidx.core:core-ktx:1.12.0'
+    implementation 'androidx.appcompat:appcompat:1.6.1'
+    implementation 'com.google.android.material:material:1.11.0'
+    implementation 'com.squareup.okhttp3:okhttp:4.12.0'
+    implementation 'com.squareup.okio:okio:3.9.0'
+    implementation platform('com.google.firebase:firebase-bom:33.1.2')
+    implementation 'com.google.firebase:firebase-messaging'
 }
